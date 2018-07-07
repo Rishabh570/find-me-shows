8095904815b1371b46eccdbe2907c5dadaf33f81
rishabh570 <www.rishabhrawat570@gmail.com>
2018-07-07 23:02:20 +0530

    Add dummy landing page.

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -3,22 +3,61 @@ import React, { Component } from 'react'
 // import { bindActionCreators } from "redux";
 // import { connect } from "react-redux";
 
+import {
+    Jumbotron, 
+    Button, 
+    Grid, 
+    Row, 
+    Col,
+    Navbar,
+    FormGroup,
+    FormControl,
+    Well,
 
-import Typography from '@material-ui/core/Typography';
-
-import Appbar from '../AppBar/Appbar.jsx'
-
+} from 'react-bootstrap';
 
 class Home extends Component {
     render() {
 
         return (
-            <div>
-                <Appbar />
-                <Typography variant="display3" gutterBottom>
-                    Home
-                </Typography>
-            </div>
+            <Grid>
+                <Row>
+                    <Navbar>
+                        <Navbar.Header>
+                            <Navbar.Brand>
+                            <a href="#home">React App</a>
+                            </Navbar.Brand>
+                            <Navbar.Toggle />
+                        </Navbar.Header>
+                        <Navbar.Collapse>
+                            <Navbar.Form pullRight>
+                            <FormGroup>
+                                <FormControl type="text" placeholder="Type what you are looking for..." />
+                            </FormGroup>{' '}
+                            <Button type="submit">Search</Button>
+                            </Navbar.Form>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </Row>
+                <Row>
+                    <Jumbotron>
+                        <h1>
+                            Welcome to Home!
+                        </h1>
+                        <p>
+                            This site is currently in development. It is the landing page of something great. A lot of work remaining.
+                        </p>
+                        <p>
+                            <Button bsSize="large" bsStyle="danger">Learn more...</Button>
+                        </p>
+                    </Jumbotron>
+                </Row>
+                <Row>
+                    <Well xs={12} md={6} >
+                        <p>This row covers some content...</p>
+                    </Well>
+                </Row>
+            </Grid>
         );   
     }
 };
